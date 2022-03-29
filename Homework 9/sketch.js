4e51c5980486238d2941a1b4a877e669d30b48d3
Grace Cassens <gracecassens@gmail.com>
2022-03-28 18:34:55 -0600

    HW9

--- Homework 9/sketch.js
+++ Homework 9/sketch.js
@@ -1,8 +1,28 @@
 function setup()
 {
-    createCanvas(400,900);
+    createCanvas(1000,1000);
 }
 function draw()
 {
-    background(237,178,58);
+    background(235,210,209);
+    console.log("hi");
+    textSize(40);
+    text('She is Beauty', 10, 30);
+    text('She is Grace',500,800);
+    circle(500,200,200);
+    ellipse(500,450,200,300);
+    rect(610,320,20,200);
+    rect(570,320,40,20);
+    rect(370,320,20,200);
+    rect(370,320,60,20);
+    circle(450,200,50);
+    circle(550,200,50);
+    line(550,250,500,250);
+    rect(550,580,20,150);
+    rect(430,580,20,150);
+    triangle(450,220,460,210,440,190);
+    triangle(550,220,560,210,540,190);
+    point(450,210);
+    point(550,210);
+    
 }
