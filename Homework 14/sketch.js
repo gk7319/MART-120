3be83c493893c7b7f625c49998342e763bdfcea8
Grace <grace.cassens@umontana.edu>
2022-04-25 08:56:10 -0600

    HW 14

--- Homework 14/sketch.js
+++ Homework 14/sketch.js
@@ -6,16 +6,57 @@ class Dog {
         this.eyeColor = eyeColor;
         this.furColor = furColor;
     }
-
     zoomies()
     {
-        text(this.name + "has the zoomies!", 200, 200);
+        text(this.name + " has the zoomies!", 200, 200);
     }
 
-    cuddle()
+    cuddles()
     {
-        text(this.name + "loves cuddles", 150, 150);
+        text(this.name + " loves cuddles", 150, 150);
+    }
+
+    chomp()
+    {
+        text(this.name + " is chomping on their enemies.", 100, 100);
     }
 }
 
-var leonberger = new Dog("Alice", "Leonberger", 120, black)
+var leonberger = new Dog("Alice", "Leonberger", 120, "black", "brown");
+
+var cockerspaniel = new Dog("Sadie", "cockerspaniel", 15, "brown", "gold");
+
+var germanShepherd = new Dog("Dune", "german shepherd", 40, "black", "brown");
+
+function display()
+{
+    zoomies()
+    {
+        text(this.name + " has the zoomies!", 200, 200);
+    }
+
+    cuddles()
+    {
+        text(this.name + " loves cuddles", 150, 150);
+    }
+
+    chomp()
+    {
+        text(this.name + " is chomping on their enemies.", 100, 100);
+    }
+}
+
+function setup()
+{
+    createCanvas (800,800);
+}
+
+function draw()
+{
+    background(242, 221, 222);
+    fill(0);
+    leonberger.cuddles();
+    cockerspaniel.zoomies();
+    germanShepherd.chomp();
+    display();
+}
